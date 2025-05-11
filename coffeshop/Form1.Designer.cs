f4ec85f5cb62d6e1807cc6e621d2ad3ef938201e
kaanyildiz26 <kaan.hua@gmail.com>
2025-05-11 13:13:14 +0300

    kapama simgesi label

--- coffeshop/Form1.Designer.cs
+++ coffeshop/Form1.Designer.cs
@@ -30,33 +30,49 @@
         {
             button1 = new Button();
             label1 = new Label();
+            panel1 = new Panel();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(86, 51);
+            button1.Location = new Point(449, 144);
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
             button1.TabIndex = 0;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(539, 176);
+            label1.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label1.Location = new Point(765, 9);
             label1.Name = "label1";
-            label1.Size = new Size(50, 20);
+            label1.Size = new Size(23, 24);
             label1.TabIndex = 1;
-            label1.Text = "label1";
+            label1.Text = "X";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.DarkCyan;
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(373, 450);
+            panel1.TabIndex = 2;
+            panel1.Paint += panel1_Paint;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
             ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
             Controls.Add(label1);
             Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.None;
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -67,5 +83,6 @@
 
         private Button button1;
         private Label label1;
+        private Panel panel1;
     }
 }
--- coffeshop/Form1.cs
+++ coffeshop/Form1.cs
@@ -6,5 +6,15 @@ namespace coffeshop
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
